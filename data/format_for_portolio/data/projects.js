b8a977aa9385cd84c07c98f73464c96a39e72a50
Matias <lit.io30303@gmail.com>
2025-07-03 17:10:46 -0400

    2025-07-03 Actualizando CV

--- data/format_for_portolio/data/projects.js
+++ data/format_for_portolio/data/projects.js
@@ -3,7 +3,7 @@ export const PROJECTS = [
     title: "Auto Actualizar Cv",
     image: null,
     date: "2025-07-03",
-    lenguajes_utilizados: {'Python': 74.0, 'JavaScript': 19.51, 'Batchfile': 6.49},
+    lenguajes_utilizados: {'Python': 73.5, 'JavaScript': 19.38, 'Batchfile': 7.11},
     topics: [],
     description: "Una aplicación en Python que automatiza la actualización de tu CV con información de tus repositorios de GitHub.",
     technologies: ["Python", "JavaScript", "Batchfile"],
