03efb72ba8c2f1d93ae9923e6e2b771536b3cc10
Matias <lit.io30303@gmail.com>
2025-07-07 20:51:49 -0400

    update estructura

---
+++ api/utils/loader.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+from typing import List
+from models.proyecto import Proyecto
+
+def cargar_proyectos_json(ruta: str = "./data/proyectos_combinados_all.json") -> List[Proyecto]:
+    ruta_archivo = Path(ruta)
+    if not ruta_archivo.exists():
+        return []
+
+    with open(ruta_archivo, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    proyectos = []
+    for p in data:
+        if p.get("sitio_web") == "":
+            p["sitio_web"] = None
+        proyectos.append(Proyecto(**p))
+    
+    return proyectos
