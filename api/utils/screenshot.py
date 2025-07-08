7dbd28456c255ef11d3716aae1b31a94b8ba4c35
Matias <lit.io30303@gmail.com>
2025-07-08 19:30:47 -0400

    update with imagenes static

--- api/utils/screenshot.py
+++ api/utils/screenshot.py
@@ -1,6 +1,6 @@
 # src/utils/screenshot.py
 import os
-import requests
+import httpx
 from dotenv import load_dotenv
 
 from pathlib import Path
@@ -10,7 +10,8 @@ load_dotenv()
 BASE_URL = "https://shot.screenshotapi.net/screenshot"
 STATIC_DIR = Path(__file__).resolve().parent.parent / "static" / "previews"
 
-def obtener_screenshot(sitio_url: str, repositorio: str) -> str:
+async def obtener_screenshot(sitio_url: str, repositorio: str) -> str:
+    """Genera un screenshot de un sitio web"""
     STATIC_DIR.mkdir(parents=True, exist_ok=True)
     archivo = STATIC_DIR / f"{repositorio}.png"
 
@@ -24,20 +25,23 @@ def obtener_screenshot(sitio_url: str, repositorio: str) -> str:
     if not isinstance(sitio_url, str):
         sitio_url = str(sitio_url)
 
-    params = {
-        "token": token,
-        "url": str(sitio_url),
-        "output": "image",
-        "file_type": "png",
-        "viewport": "1440x1024",
-        "full_page": "true"
-    }
+    async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
+        response = await client.get(BASE_URL, params={
+            "token": token,
+            "url": str(sitio_url),
+            "output": "image",
+            "file_type": "png",
+            "viewport": "1440x1024",
+            "full_page": "true"
+        },
+        follow_redirects=True,
+        timeout=httpx.Timeout(60.0)
+        )
 
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code != 200:
-        raise Exception("Error al generar screenshot")
+        if response.status_code != 200:
+            raise Exception(f"Error al generar screenshot: {response.status_code} - {response.text}")
 
-    with open(archivo, "wb") as f:
-        f.write(response.content)
+        with open(archivo, "wb") as f:
+            f.write(response.content)
 
     return str(archivo)
