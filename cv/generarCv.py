f43066be49d7c0c35e80549f54076ea232129cd6
Matias <lit.io30303@gmail.com>
2025-07-11 09:52:39 -0400

    2025-07-11 Actualizando CV

--- cv/generarCv.py
+++ cv/generarCv.py
@@ -10,14 +10,47 @@ def generar_cv(proyectos_destacados: list, output_path: str="./data/CV_Matias_Pe
 
     #------------------------------------------
     # Info Personal
-    pdf.section_title("Información de Contacto")
-    pdf.multi_section([
-        "Puerto Varas - Chile",
-        "matiaspereznauto@gmail.com",
-        "+56 9 7547 5781",
-        "GitHub: www.github.com/mtsprznto",
-        "LinkedIn: www.linkedin.com/in/matiaspereznauto/"
-    ])
+    # pdf.section_title("Información de Contacto")
+    # pdf.multi_section([
+    #     "Puerto Varas - Chile",
+    #     "matiaspereznauto@gmail.com",
+    #     "+56 9 7547 5781",
+    #     "GitHub: www.github.com/mtsprznto",
+    #     "LinkedIn: www.linkedin.com/in/matiaspereznauto/"
+    # ])
+    pdf.set_font("Arial", "", 10)
+    pdf.cell(0,10,"Puerto Varas - Chile • www.linkedin.com/in/matiaspereznauto/ • +569 75475781 • matiaspereznauto@gmail.com", ln=True, align="C")
+    #--------------------
+    # DESCRIPCION
+    pdf.set_font("Arial", "H", 10)
+    pdf.cell(0,10,"Desarrollador de software con experiencia en Python, JavaScript, Next.js, PHP , SQL. Apasionado por crear soluciones eficientes, seguras y optimizadas, con enfoque en interfaces y mejores prácticas. Busco aportar en entornos dinámicos e innovadores.", ln=True, align="R")
+
+    #------------------------------------------
+    # Experencia profesional
+    pdf.section_title("Experencia profesional")
+    pdf.set_font("Arial", "B", 10)
+    page_width = pdf.w - 2 * pdf.l_margin  # ancho usable del documento
+
+    right_text = "Puerto Varas, Chile"
+    right_text_width = pdf.get_string_width(right_text)
+    pdf.set_x(pdf.l_margin)
+    pdf.cell(page_width - right_text_width, 6, "")  # espacio hasta el texto
+    pdf.cell(right_text_width, 6, right_text, ln=0, align="R")
+
+    # Texto alineado a la izquierda (en la misma línea)
+    left_text = "Desarrollador FullStack - Gestpass S.A"
+    pdf.set_xy(pdf.l_margin, pdf.get_y())  # volver al inicio horizontal
+    pdf.cell(page_width, 6, left_text, ln=1, align="L")
+
+
+
+    #------------------------------------------
+    # Proyectos 
+    pdf.section_title("Proyectos Destacados")
+    #print(f"Proyectos destacados: {proyectos_destacados}")
+    # pdf.multi_section(proyectos_destacados)
+    for proyecto in proyectos_destacados:
+        pdf.render_proyecto(proyecto)
 
     #------------------------------------------
     # Educación
@@ -27,13 +60,7 @@ def generar_cv(proyectos_destacados: list, output_path: str="./data/CV_Matias_Pe
         "Técnico Administración de Empresas RRHH, Colegio Felmer Niklitschek, 2017"
     ])
 
-    #------------------------------------------
-    # Proyectos
-    pdf.section_title("Proyectos Destacados")
-    #print(f"Proyectos destacados: {proyectos_destacados}")
-    # pdf.multi_section(proyectos_destacados)
-    for proyecto in proyectos_destacados:
-        pdf.render_proyecto(proyecto)
+    
 
     #------------------------------------------
     # Tecnologías y Conocimientos
