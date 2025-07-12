63bef843296718882bb9ae7b6741d58de6d58817
Matias <lit.io30303@gmail.com>
2025-07-11 22:15:50 -0400

    2025-07-11 Actualizando CV

--- data/format_for_portolio/data/projects.js
+++ data/format_for_portolio/data/projects.js
@@ -1,9 +1,31 @@
 export const PROJECTS = [
   {
+    title: "Academ Prod",
+    image: null,
+    date: "2025-07-11",
+    lenguajes_utilizados: {'TypeScript': 98.21, 'CSS': 1.62, 'JavaScript': 0.17},
+    topics: ["fulls", "fullstack", "nextjs"],
+    description: "Academ. plataforma es un entorno amigable para compartir conocimiento y aprender de manera organizada. Los usuarios pueden registrarse y acceder a diversas funcionalidades dependiendo de su rol dentro del sistema",
+    technologies: ["TypeScript", "CSS", "JavaScript"],
+    url_demo: "https://academ-prod.vercel.app",
+    url_codigo: "https://github.com/mtsprznto/academ-prod"
+  },
+  {
+    title: "Automatizaciones",
+    image: null,
+    date: "2025-07-11",
+    lenguajes_utilizados: {'Python': 99.02, 'Batchfile': 0.98},
+    topics: ["automation", "python", "scraping"],
+    description: "Este repositorio contiene una colección de proyectos de automatización y scraping desarrollados en Python. Cada proyecto está diseñado para optimizar procesos específicos y extraer datos de manera eficiente.",
+    technologies: ["Python", "Batchfile"],
+    url_demo: "",
+    url_codigo: "https://github.com/mtsprznto/Automatizaciones"
+  },
+  {
     title: "Auto Actualizar Cv",
     image: null,
     date: "2025-07-11",
-    lenguajes_utilizados: {'Python': 79.48, 'JavaScript': 10.59, 'Batchfile': 9.93},
+    lenguajes_utilizados: {'Python': 79.48, 'JavaScript': 10.58, 'Batchfile': 9.93},
     topics: ["automatizaciones", "fastapi", "python"],
     description: "Una solución automatizada para mantener tu CV profesional siempre actualizado, extrayendo información directamente de tus repositorios de GitHub y generando un documento PDF listo para compartir.",
     technologies: ["Python", "JavaScript", "Batchfile"],
@@ -13,7 +35,7 @@ export const PROJECTS = [
   {
     title: "Portafolio Minimalist",
     image: null,
-    date: "2025-07-09",
+    date: "2025-07-11",
     lenguajes_utilizados: {'JavaScript': 91.76, 'TypeScript': 4.95, 'CSS': 1.96, 'HTML': 1.33},
     topics: ["react", "react-vite", "vite"],
     description: "Diseñé y desarrollé una aplicación web moderna utilizando React para una estructura modular y eficiente, complementada con TailwindCSS para una interfaz rápida y optimizada",
@@ -31,28 +53,6 @@ export const PROJECTS = [
     technologies: ["TypeScript", "CSS", "JavaScript"],
     url_demo: "https://coffdream.vercel.app",
     url_codigo: "https://github.com/mtsprznto/CoffeDream"
-  },
-  {
-    title: "Automatizaciones",
-    image: null,
-    date: "2025-07-04",
-    lenguajes_utilizados: {'Python': 99.02, 'Batchfile': 0.98},
-    topics: ["automation", "python", "scraping"],
-    description: "Este repositorio contiene una colección de proyectos de automatización y scraping desarrollados en Python. Cada proyecto está diseñado para optimizar procesos específicos y extraer datos de manera eficiente.",
-    technologies: ["Python", "Batchfile"],
-    url_demo: "",
-    url_codigo: "https://github.com/mtsprznto/Automatizaciones"
-  },
-  {
-    title: "Academ Prod",
-    image: null,
-    date: "2025-07-04",
-    lenguajes_utilizados: {'TypeScript': 98.18, 'CSS': 1.64, 'JavaScript': 0.18},
-    topics: ["fulls", "fullstack", "nextjs"],
-    description: "Academ. plataforma es un entorno amigable para compartir conocimiento y aprender de manera organizada. Los usuarios pueden registrarse y acceder a diversas funcionalidades dependiendo de su rol dentro del sistema",
-    technologies: ["TypeScript", "CSS", "JavaScript"],
-    url_demo: "https://academ-prod.vercel.app",
-    url_codigo: "https://github.com/mtsprznto/academ-prod"
   },
   {
     title: "Gestpass",
