79ecb603ca6ae70b27960ac099604d24fce6c0f7
Matias <lit.io30303@gmail.com>
2025-07-23 10:53:47 -0400

    Actualizando CV

--- get_all_repositorios.py
+++ get_all_repositorios.py
@@ -35,12 +35,12 @@ def main():
         proyectos_lenguajes = [github.get_languages_for_repo(username, r.get("name")) for r in sorted(repositories, key=lambda x: x['updated_at'], reverse=True)]
 
         proyectos_combinados = combinar_repos(proyectos_cv, proyectos_about, proyectos_lenguajes)
-        #print("Proyectos combinados:", proyectos_combinados)
+        #print("Proyectos combinados:", proyectos_combinados[0]["titulo"])
         proyectos_filtrados = [p for p in proyectos_combinados if p["titulo"] != "MTSPRZNTO (None)"]
         
         guardar_json_repositorios(proyectos_filtrados, "./data/proyectos_combinados_all.json")
         
-        generar_cv(proyectos_destacados=proyectos_combinados, output_path='./data/CV_Matias_Perez_Nauto_all.pdf')
+        generar_cv(proyectos_destacados=proyectos_filtrados, output_path='./data/CV_Matias_Perez_Nauto_all.pdf')
         # Save repositories to a JSON file for further processing
         guardar_json_repositorios(repositories, "./data/repositories_all.json")
 
